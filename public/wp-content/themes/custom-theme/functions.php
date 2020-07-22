4c639559eb4b2edcfa040bc9a38129609214bb76
nickzou <z.nick00@gmail.com>
2020-07-22 17:19:41 -0400

    Formatting

--- public/wp-content/themes/custom-theme/functions.php
+++ public/wp-content/themes/custom-theme/functions.php
@@ -3,5 +3,5 @@
     require_once(get_template_directory() . '/functions/global/enqueue_global_resources.php');
     require_once(get_template_directory() . '/functions/global/theme_setup.php');
     require_once(get_template_directory() . '/functions/global/get_component.php');
-require_once(get_template_directory() . '/functions/global/open_svg.php');
+    require_once(get_template_directory() . '/functions/global/open_svg.php');
 ?>
